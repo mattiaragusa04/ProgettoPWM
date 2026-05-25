d7f0ccfa437d458baca334d2bedcf307a888971b
mattiaragusa04 <ragusamatt@hotmail.com>
2026-05-25 10:15:09 +0200

    modifica nome progetto

--- PAwerUP/src/app/app.spec.ts
+++ PAwerUP/src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, prog_pwm');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, PAwerUP');
   });
 });
